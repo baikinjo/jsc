32beb9c531c9e88ef32b6c6b8b843780a7e40c9f
Injo Baik <baikinjo.28@gmail.com>
2019-07-24 03:08:09 -0700

    server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ mongoose
 app.use('/api/items', items)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
